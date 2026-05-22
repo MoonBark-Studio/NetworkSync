40e4344b89ec20dd68dbde1eeaca83f6522eb244
Chris' Tutorials <christutorialsyt@gmail.com>
2026-05-22 11:44:32 -0400

    chore(networksync): update E2E test mocks and csproj

--- Tests/ThistletideE2ETests/ThistletideMocks.cs
+++ Tests/ThistletideE2ETests/ThistletideMocks.cs
@@ -1,4 +1,6 @@
-﻿namespace MoonBark.NetworkSync.Tests.ThistletideE2E.ThistletideMock;
+namespace MoonBark.NetworkSync.Tests.ThistletideE2E.ThistletideMock;
+
+using MoonBark.Framework.Types;
 
 /// <summary>
 /// Mock implementation of Thistletide's TrackedPlacementOccupancyMap for testing.
@@ -95,29 +97,4 @@ public struct PlacementCellOccupancy
     };
 }
 
-/// <summary>
-/// Simple 2D vector for integers matching MoonBark.Framework.Types.CoreVector2I.
-/// </summary>
-public struct CoreVector2I
-{
-    public int X { get; set; }
-    public int Y { get; set; }
 
-    public CoreVector2I(int x, int y)
-    {
-        X = x;
-        Y = y;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is CoreVector2I other)
-            return X == other.X && Y == other.Y;
-        return false;
-    }
-
-    public override int GetHashCode() => HashCode.Combine(X, Y);
-
-    public static bool operator ==(CoreVector2I left, CoreVector2I right) => left.X == right.X && left.Y == right.Y;
-    public static bool operator !=(CoreVector2I left, CoreVector2I right) => !(left == right);
-}
